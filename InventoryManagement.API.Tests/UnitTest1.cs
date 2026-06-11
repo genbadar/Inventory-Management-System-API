476a44b972b22b28edac821775763030c8dd5092
Badar Tagar <genbadar@gmail.com>
2026-06-12 03:17:53 +0500

    Add Tests projects for all projects for unit and application testing

---
+++ InventoryManagement.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace InventoryManagement.API.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
